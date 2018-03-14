4ade7973d2cab0108e3b4be5e3ff5de6e762c77e
xuanquang1999 <xuanquang1999@gmail.com>
2018-03-14 15:27:45 +0700

    Changed 'char path[]' to 'string path', add file not found detection and comma printing in OutputStudentToAnotherFile

--- staff_students.cpp
+++ staff_students.cpp
@@ -1,18 +1,18 @@
 #include "staff_students.h"
 
-void ImportStudentFromFile(char path[], vector<User> &users)
+void ImportStudentFromFile(string path, vector<User> &users)
 {
 	ifstream fin;
 	fin.open(path);
 
 	if (!fin.is_open())
 	{
-		cout << "File not open";
+		cout << "File '" << path << "' not open" << endl;;
 		return;
 	}
 	string tmp, _class;
 	getline(fin, tmp, ','); //tmp=Class
-	getline(fin, _class, ','); //Put class into _class
+	getline(fin, _class, '\n'); //Put class into _class
 	getline(fin, tmp, '\n'); //put second line into tmp
 	while (!fin.eof())
 	{
@@ -27,13 +27,18 @@ void ImportStudentFromFile(char path[], vector<User> &users)
 	fin.close();
 }
 
-void OutputStudentToAnotherFile(char path[], vector<User> users)
+void OutputStudentToAnotherFile(string path, vector<User> users)
 {
 	ofstream fout;
 	fout.open(path);
+	if (!fout.is_open())
+	{
+		cout << "File '" << path << "' not open" << endl;
+		return;
+	}
 	for (int i = 0; i < users.size(); ++i)
 	{
-		fout << users[i]._class << ' ' << users[i].username << ' ' << users[i].fullName << endl;
+		fout << users[i]._class << ',' << users[i].username << ',' << users[i].fullName << endl;
 	}
 	fout.close();
 }
