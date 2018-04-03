e35f6b5b6f059b9b6c9a037e99eed16a3b871c22
xuanquang1999 <xuanquang1999@gmail.com>
2018-04-03 09:11:05 +0700

    Add staff_course files

---
+++ staff_course.cpp
@@ -0,0 +1,9 @@
+/*
+Implementation for use-cases of academic staff about course managing
+*/
+
+#include "staff_course.h"
+
+void ImportCourseFromFile(string path, vector<Course> &courses) {
+
+}
---
+++ staff_course.h
@@ -0,0 +1,12 @@
+/*
+Header for use-cases of academic staff about course managing
+*/
+
+#ifndef STAFF_COURSE_H
+#define STAFF_COURSE_H
+
+#include "sms.h"
+
+void ImportCourseFromFile(string path, vector<Course> &courses);
+
+#endif // STAFF_COURSE_H
